4ec4a87775180484a2379d851a83f0aabe3be30b
Craig Booker <craig@arcadiasoftworks.com>
2020-02-11 17:59:11 -0600

    Basic exercise to learn more about state in ReactJS.

---
+++ src/Clicker.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Clicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 1 };
+    this.genRandom = this.genRandom.bind(this);
+  }
+  genRandom() {
+    // Pick a number 1-10
+    let rand = Math.floor(Math.random() * 10) + 1;
+    // Update state with a new rand
+    this.setState({ num: rand });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Number is: {this.state.num}</h1>
+        {this.state.num === 7 ? (
+          <h2>YOU WIN!</h2>
+        ) : (
+          <button onClick={this.genRandom}>Random Number</button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Clicker;
